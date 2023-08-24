58eb19a538fc01d2d4c6973cf5edc0d4706d196f
Yersultan <ersultansansyzbaj@gmail.com>
2023-08-24 12:47:55 +0600

    feat: add components postwidged, categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
-import { PostCard } from "@/components";
+import { Categories, PostCard, PostWidget } from "@/components";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <PostCard />
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
+          <PostCard />
+        </div>
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- services/index.ts
+++ services/index.ts
@@ -1,1 +1,44 @@
-export {}
+import { gql, request } from "graphql-request";
+
+export const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+
+const gqlPosts = gql`
+  query Assets {
+    postsConnection {
+      edges {
+        cursor
+        node {
+          author {
+            bio
+            name
+            id
+            photo {
+              url
+            }
+          }
+          createdAt
+          slug
+          title
+          excerpt
+          featuredImage {
+            url
+          }
+          categories {
+            name
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const getPosts = async () => {
+  try {
+    const res = await request(graphqlAPI, gqlPosts);
+
+    return res.postsConnection.edges;
+  } catch (error) {
+    console.error(error);
+  }
+};
