fa23c20e5018d0c832586880ca937d1d81dd69e6
Yersultan <ersultansansyzbaj@gmail.com>
2023-09-04 14:35:12 +0600

    feat: add layout

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { PostCard, Categories, Spinner, Row } from "@/components";
+import { PostCard, Categories, Spinner, Row, Footer } from "@/components";
 import useGraphQLRequest from "@/services/useGraphQLRequest";
 
 interface CategoryPostProps {
@@ -53,21 +53,23 @@ const CategoryPost: React.FC<CategoryPostProps> = ({ params }) => {
   // }
 
   return (
-    <div className="container mx-auto md:px-10 px-5 mb-8">
-      <Row text={categoriesName[0]} styles="mb-5" />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-        <div className="col-span-1 lg:col-span-8">
-          {posts.map((post, index) => (
-            <PostCard key={index} post={post.node} />
-          ))}
-        </div>
-        <div className="col-span-1 lg:col-span-4">
-          <div className="relative lg:sticky top-8">
-            <Categories />
+    <>
+      <div className="container mx-auto md:px-10 px-5 mb-8">
+        <Row text={categoriesName[0]} styles="mb-5" />
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+          <div className="col-span-1 lg:col-span-8">
+            {posts.map((post, index) => (
+              <PostCard key={index} post={post.node} />
+            ))}
+          </div>
+          <div className="col-span-1 lg:col-span-4">
+            <div className="relative lg:sticky top-8">
+              <Categories />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -95,7 +95,6 @@ export default function Home() {
           />
         ) : null}
       </div>
-      <Footer />
     </>
   );
 }
